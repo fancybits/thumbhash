26bc7c925547e5fa60a6fe84e8fd8e882feb848e
Eric Lindvall <eric@5stops.com>
2024-01-13 11:49:25 -0800

    Add visionOS support

--- swift/ThumbHash.swift
+++ swift/ThumbHash.swift
@@ -555,7 +555,7 @@ func thumbHashToImage(hash: Data) -> NSImage {
 }
 #endif
 
-#if os(iOS) || os(tvOS)
+#if os(iOS) || os(tvOS) || os(visionOS)
 import UIKit
 
 func imageToThumbHash(image: UIImage) -> Data {
